214f1844c1c562f4e6f07e50547e21cef5c82c28
Qin <aries.duanmu@gmail.com>
2018-01-26 13:28:06 +0800

    small update

--- lsb.py
+++ lsb.py
@@ -23,19 +23,17 @@ def extract(image_filename,
     for row in range(row_data):
         for column in range(column_data):
             r, g, b, a = conv.getpixel((column, row))
+            planes = {"R":[r, red_plane], 
+                      "G":[g, green_plane], 
+                      "B":[b, blue_plane],
+                      "A":[a, alpha_plane]}
+
             for current_bit in bit_plane_order:
-                if current_bit == "R" and len(red_plane) > 0:
-                    for r_plane in red_plane:
-                        v.append((r & (1 << r_plane)) >> r_plane)
-                elif current_bit == "G" and len(green_plane) > 0:
-                    for g_plane in green_plane:
-                        v.append((g & (1 << g_plane)) >> g_plane)
-                elif current_bit == "B" and len(blue_plane) > 0:
-                    for b_plane in blue_plane:
-                        v.append((b & (1 << b_plane)) >> b_plane)
-                elif current_bit == "A" and len(alpha_plane) > 0:
-                    for a_plane in alpha_plane:
-                        v.append((a & (1 << a_plane)) >> a_plane)
+                current_plane = planes[current_bit][1]
+                if len(current_plane) > 0:
+                    for plane in current_plane:
+                        v.append((planes[current_bit][0] & (1 << plane)) >> plane)
+                
     return assemble(v)
 
 def assemble(v):
@@ -49,7 +47,7 @@ def assemble(v):
 
 if __name__ == "__main__":
     image_filename = "BAND.JPG"
-    print(extract(image_filename, [], [], [], [] ))
+    print(extract(image_filename, [], [0], [0], [0] ))
 
     
             
